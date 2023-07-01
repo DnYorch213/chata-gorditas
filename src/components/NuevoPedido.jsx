2345b76916642b0234e6746651e2cdf3f5945bd6
DnYorch213 <joloare@hotmail.com>
2023-06-30 20:32:34 -0600

    mi segundo comit

--- src/components/NuevoPedido.jsx
+++ src/components/NuevoPedido.jsx
@@ -1,5 +1,7 @@
 import { useReducer } from "react";
-import productReducer, { initialProductState } from "../reducers/productReducer";
+import productReducer, {
+  initialProductState,
+} from "../reducers/productReducer";
 import types from "../types";
 import "../index.css";
 
@@ -12,143 +14,145 @@ const NuevoPedido = () => {
 
   return (
     <div className="container">
-      
-
       {/* TITULO DE LA CABECERA */}
       <div className="header__title">
         <h2 className="header__title--text">
-          G o r d i t a s  &nbsp;&nbsp; L a &nbsp;  C h a t i s !!
+          G o r d i t a s &nbsp;&nbsp; L a &nbsp; C h a t i s !!
         </h2>
       </div>
-      
-        
 
-      <article className="content ">         
-                  
-          <div className="title_carrito">
-      <h2>Carrito</h2>
+      <article className="content ">
+        <div className="title_carrito">
+          <h2>Carrito</h2>
 
-      <span >
-        Total= ${" "}
-        {cart?.reduce(
-          (amount, product) => product.price * product.quantity + amount,
-          0
-        )}
-        .00
-      </span>
-      </div>
-      
-      <p>Gorditas</p>
-      
-        <div className="content__burgers">
-            {products.map((product) => (
-              <div key={product.id} className="burger" style={{ border: "thin solid cyan", color: "yellow"}} 
-                
-              onClick={() =>
-                dispatch({
-                  type: types.productAddToCart,
-                  payload: product,
-                })
-                
-              } >    
-                        
-                               
-                <h3>{product.title}</h3>
-                
-                <h5>${product.price}.00</h5>  
-                                
-                  
-              </div>
-              
-            ))}
-          
+          <span>
+            Total= ${" "}
+            {cart?.reduce(
+              (amount, product) => product.price * product.quantity + amount,
+              0
+            )}
+            .00
+          </span>
         </div>
 
-          <p>Taquitos de Harina</p>
-        <div className="content__burgers">
-            {products2.map((product) => (
-              <div key={product.id} className="burger" style={{border: "thin solid lavender", color: "pink"}} 
-               onClick={() =>
-                dispatch({
-                  type: types.productAddToCart,
-                  payload: product,
-                })
-              }>
-                <h3>{product.title}</h3>
-                
-                <h5>${product.price}.00</h5>
-              
-              </div>
-            ))}
-          
-        </div>
+        <p>Gorditas</p>
 
-          <p>Bebidas</p>
         <div className="content__burgers">
-            {producto3.map((product) => (
-              <div key={product.id} className="burger" style={{ border: "thin solid cyan", color:"whitesmoke" }}
+          {products.map((product) => (
+            <div
+              key={product.id}
+              className="burger"
+              style={{ border: "thin solid cyan", color: "yellow" }}
               onClick={() =>
                 dispatch({
                   type: types.productAddToCart,
                   payload: product,
                 })
               }
-            >                
-                <h3>{product.title}</h3>
-                
-                <h5>${product.price}.00</h5>                
-               
-              </div>
-              
-            ))}
-          
+            >
+              <h3>{product.title}</h3>
+
+              <h5>${product.price}.00</h5>
+            </div>
+          ))}
         </div>
-      </article>      
-      
-      
-      <article className="container_carrito">
+
+        <p>Taquitos de Harina</p>
+        <div className="content__burgers">
+          {products2.map((product) => (
+            <div
+              key={product.id}
+              className="burger"
+              style={{ border: "thin solid lavender", color: "pink" }}
+              onClick={() =>
+                dispatch({
+                  type: types.productAddToCart,
+                  payload: product,
+                })
+              }
+            >
+              <h3>{product.title}</h3>
+
+              <h5>${product.price}.00</h5>
+            </div>
+          ))}
+        </div>
+
+        <p>Bebidas</p>
+        <div className="content__burgers">
+          {producto3.map((product) => (
+            <div
+              key={product.id}
+              className="burger"
+              style={{
+                border: "thin solid cyan",
+                color: "whitesmoke",
+                
+              }}
+              onClick={() =>
+                dispatch({
+                  type: types.productAddToCart,
+                  payload: product,
+                })
+              }
+            >
+              <h3>{product.title}</h3>
+
+              <h5>${product.price}.00</h5>
+            </div>
+          ))}
+        </div>
+        <div>
         <p>Pedido</p>
         {cart.map((product) => (
-          
           <div
             key={product.id}
-            style={{ border: "thin solid lime", background:"aquamarine" }}
+             
+            style={{ border: "thin solid yellow" }}
           >
-            
-            <div className="content-carrito">
-            {product.title}
-            <h5>${product.price}.00</h5> x {product.quantity} = $
-            {product.price * product.quantity}.00
-            
-            <div className="botones">
-            <button className="removeone"
-              onClick={() =>
-                dispatch({
-                  type: types.productRemoveOneFromCart,
-                  payload: product.id,
-                })
-              }
-            >
-              Remove one
-            </button>
-            <button className="removeall"
-              onClick={() =>
-                dispatch({
-                  type: types.productRemoveFromCart,
-                  payload: product.id,
-                })
-              }
-            >
-              Remove all
-            </button>
+            <div className="container_carrito">
+              {product.title}
+              <h5>${product.price}.00</h5> <h5>x&nbsp;&nbsp; {product.quantity} </h5>= &nbsp;&nbsp;&nbsp; $
+              {product.price * product.quantity}.00
+              <div className="botones">
+                <button
+                  className="removeone"
+                  onClick={() =>
+                    dispatch({
+                      type: types.productRemoveOneFromCart,
+                      payload: product.id,
+                    })
+                  }
+                >
+                  Eliminar
+                </button>{/* <button
+                  className="removeall"
+                  onClick={() =>
+                    dispatch({
+                      type: types.productRemoveFromCart,
+                      payload: product.id,
+                    })
+                  }
+                >
+                  Remove all
+                </button> */}
+                
+              </div>
             </div>
-           </div>
           </div>
         ))}
+        </div>
       </article>
-     
-    </div> 
-);
+        <div className="total2">
+            Total= ${" "}
+            {cart?.reduce(
+              (amount, product) => product.price * product.quantity + amount,
+              0
+            )}
+            .00
+          </div>
+    </div>
+  );
 };
-     
+
 export default NuevoPedido;
